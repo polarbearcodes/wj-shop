07094a646f47ea628ed78f93dc9c15e67dcae540
Wenjun Chua <wenjunchua99@gmail.com>
2023-09-24 23:06:41 +0800

    [WJ] set up BE

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import {Row, Col} from "react-bootstrap";
-import products from "../products"
+import React, {useEffect,useState} from 'react'
+import {Row, Col} from "react-bootstrap"; 
 import Product from "../components/Product";
+import axios from 'axios';
 function HomeScreen() {
+  const [products,setProducts] = useState([]);
+
+  useEffect(()=>{
+    const fetchProducts = async ()=>{
+      const {data} = await axios.get('/api/products')
+      setProducts(data);
+    }
+    fetchProducts();
+  },[])
   return (
     <>
     <h1>Latest Products</h1>
