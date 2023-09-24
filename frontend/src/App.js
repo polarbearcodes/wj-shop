5a8fe09304e064c4ed32e31fe2283d7f96c8452a
Wenjun Chua <wenjunchua99@gmail.com>
2023-09-24 19:50:54 +0800

    [WJ] starting up frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import { Container } from "react-bootstrap";
 import React from "react";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 function App() {
-  return <h1>My first page</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import {LinkContainer} from "react-router-bootstrap";
+import logo from "../assets/logo.jpg"
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to = '/'>
+          <Navbar.Brand>
+            <img src={logo} height = "100px" width= "100px" alt = 'StuffedCompanions'/>
+            </Navbar.Brand>
+            </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+              <Nav.Link>
+                <FaShoppingCart />
+                Cart
+              </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to = "/login">
+              <Nav.Link>
+                <FaUser />
+                Sign In
+              </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
